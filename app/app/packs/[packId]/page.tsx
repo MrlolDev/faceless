afadd5b4768a24424608b98b569ed6734c21ded0
MrlolDev <mrlol.yt.oficial@gmail.com>
2025-01-01 16:30:55 +0100

    refactor: update type definition for params in PrivatePage component

--- app/app/packs/[packId]/page.tsx
+++ app/app/packs/[packId]/page.tsx
@@ -3,7 +3,11 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import PackPage from "@/components/platform/Packs/PackPage";
 
-export default async function PrivatePage(props: { params: Promise<any> }) {
+export default async function PrivatePage(props: {
+  params: Promise<{
+    packId: string;
+  }>;
+}) {
   const params = await props.params;
   const supabase = await createClient();
   const { data, error } = await supabase.auth.getUser();
